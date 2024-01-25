9573d66b31bb6d8a9fc5aeeeae61307590576785
mrskeletone <mr_skeletone_1000@mail.ru>
2024-01-25 19:14:56 +0300

    ``1

--- src/three.java
+++ src/three.java
@@ -1,15 +1,18 @@
 
 public class three {
-    public static double diamBaka=5,visotaBaka=25,plotnosty=1000;
+    public static double D=5,H=25,plotnosty=1000;
 
     public static void main(String[] args) {
-        double nozzleDiameter = 0.65*Math.sqrt(diamBaka/visotaBaka);
-        double P=plotnosty*10*visotaBaka;
+        //D = √((4*A)/π)
+        //D = (√((P - Pa) / ρ) * V)
+        double nozzleDiameter = 0.65*Math.sqrt(D/H);
+        double g=9.8;
+        double P=plotnosty*g*H;
         double v=Math.sqrt((2*(P-Math.pow(10,5)))/plotnosty);
-        int N=(int)Math.ceil((Math.PI*diamBaka)/(nozzleDiameter*0.3*diamBaka));
-        double h=visotaBaka-0.3*diamBaka;
-        double angle=Math.atan(h/(diamBaka/2));
+        int N=(int)Math.ceil((Math.PI*H)/(nozzleDiameter*0.3*H));
+        double h=H-0.3*D;
+        double angle=Math.atan(h/(D/2));
         double Q=(N*Math.PI*Math.pow(nozzleDiameter,2)*v)/4;
-        System.out.println(String.format("d=%.4f\nP=%.4f\nv=%.4f\nN=%d\nh=%.4f\nangle=%.4f\nQ=%.4f",nozzleDiameter,P,v,N,visotaBaka-h,angle,Q));
+        System.out.println(String.format("d=%.4f\nP=%.4f\nv=%.4f\nN=%d\nh=%.4f\nangle=%.4f\nQ=%.4f",nozzleDiameter,P,v,N,H-h,angle,Q));
     }
 }
